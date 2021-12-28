09e07c1b13baa0fbcd02ec1102d8d3522136d97b
Pedrinhonitz <pedrohenriquekleinphg@gmail.com>
2021-12-28 11:02:12 -0300

    1003 - Soma Simples

---
+++ C/1003 - Soma Simples.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main() {
+    int a, b, sum;
+    
+    scanf("%d" ,&a);
+    scanf("%d" ,&b);
+    sum = a + b;
+    
+    printf("SOMA = %d\n", sum);
+    
+    return 0;
+}
