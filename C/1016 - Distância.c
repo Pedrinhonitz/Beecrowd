7d2fe678f0494ca5e71a8716eb3a2fffe72a00a9
Pedrinhonitz <pedrohenriquekleinphg@gmail.com>
2021-12-28 14:33:35 -0300

    1016 - Distância

---
+++ C/1016 - Distância.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    int n;
+    
+    scanf("%d", &n);
+
+    n = n * 2;
+    
+    printf("%d minutos\n", n);
+
+    return 0;
+}
