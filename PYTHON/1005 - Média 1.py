2036d34bbef16f0c11d1c7c0bae8a5730dca3d2e
Pedrinhonitz <pedrohenriquekleinphg@gmail.com>
2021-12-29 16:28:47 -0300

    1005 - Média 1

---
+++ PYTHON/1005 - Média 1.py
@@ -0,0 +1,9 @@
+n1 = float(input())
+n2 = float(input())
+
+if (n1, n2 < 11) or (n1, n2 >= 1):
+    m1 = n1 * 3.5
+    m2 = n2 * 7.5
+    sum1 = m1 + m2
+    sum2 = sum1 / 11
+    print(f"MEDIA = {sum2:.5f}")
