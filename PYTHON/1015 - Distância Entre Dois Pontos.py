08bc111b8a23b31dc81ef63403ec64eb0d6b78e1
Pedro Henrique <pedrohenriquekleinphg@gmail.comgit config --global user.name Pedrinhonitz>
2022-01-02 14:22:41 -0300

    1015 - Distância Entre Dois Pontos

---
+++ PYTHON/1015 - Distância Entre Dois Pontos.py
@@ -0,0 +1,9 @@
+import math
+
+n1 = input().split(" ")
+n2 = input().split(" ")
+
+sum1,sum2 = n1
+sum3,sum4 = n2
+
+distancia = math.sqrt(((float(x2) - float(x1))*(float(x2) - float(x1))) + ((float(sum4)-float(sum2)) *(float(sum4)-float(sum2))
