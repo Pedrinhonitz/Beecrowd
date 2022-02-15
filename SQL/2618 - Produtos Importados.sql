3a89b12c7f34b04227b513a05e8ac6cf741cbf31
/-\ - Pedro Henrique Klein <pedrohenriquekleinphg@gmail.com>
2022-02-15 18:29:18 -0300

    2618 - Produtos Importados

---
+++ SQL/2618 - Produtos Importados.sql
@@ -0,0 +1,1 @@
+SELECT pd.name, pv.name, c.name FROM providers AS pv JOIN products AS pd ON pv.id = pd.id_providers JOIN categories AS c ON c.id = pd.id_categories WHERE pv.name like 'Sansul SA' and c.name like 'Imported';
