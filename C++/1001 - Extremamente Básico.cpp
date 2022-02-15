5fe727688e618d0293e79aeaae551457c36da0cb
/-\ - Pedro Henrique Klein <pedrohenriquekleinphg@gmail.com>
2022-02-15 18:33:27 -0300

    1001 - Extremamente Básico

---
+++ C++/1001 - Extremamente Básico.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <new>
+
+using namespace std;
+
+int main() {
+    int x, y, sum;
+
+    cin >> x;
+    cin >> y;
+
+    sum = x + y;
+
+    cout << "X = ";
+    cout << sum;
+    cout << endl;
+
+    return 0;
+}
