e8225499c77248cf63ca790617f31f6cdd80226f
/-\ - Pedro Henrique Klein <pedrohenriquekleinphg@gmail.com>
2022-02-16 09:44:33 -0300

    1004 - Produto Simples

---
+++ C++/1004 - Produto Simples.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <new>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    int x, y, sum;
+
+    cin >> x; 
+    cin >> y;
+
+    sum = x * y;
+
+    cout << "PROD = " << sum << endl;
+
+    return 0;
+}
