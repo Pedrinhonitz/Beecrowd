161a0a71496075ed52c182864c3a140ef30b93a5
/-\ - Pedro Henrique Klein <pedrohenriquekleinphg@gmail.com>
2022-02-16 09:43:18 -0300

    1003 - Soma Simples

---
+++ C++/1003 - Soma Simples.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <new>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    int x, y, sum;
+
+    cin >> x;
+    cin >> y;
+
+    sum = x + y;
+
+    cout << "SOMA = " << sum << endl;
+
+    return 0;    
+}
